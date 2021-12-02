d6feb5e74a87fce33bd3d24e5eba78e6e1433d76
Mitch Nenn <mitch.nenn@earthlink.net>
2021-12-01 22:36:11 -0800

    Ok, day one complete!

--- day-one.fs
+++ day-one.fs
@@ -3,7 +3,7 @@ module day_one
 open System.IO
 
 let countIncreasingReadings (filename:string) : int =
-    let readings = File.ReadAllLines(Path.Combine(__SOURCE_DIRECTORY__, filename))
+    let readings = File.ReadAllLines(Path.Combine(__SOURCE_DIRECTORY__, "Data", filename))
     match readings with
     | [||] -> 0
     | _ -> readings
@@ -14,8 +14,29 @@ let countIncreasingReadings (filename:string) : int =
            |> Seq.map snd
            |> Seq.head
 
+let countIncreasingSlidingWindows (filename:string) =
+    let readings = File.ReadAllLines(Path.Combine(__SOURCE_DIRECTORY__, "Data", filename))
+    match readings with
+    | [||] -> 0
+    | _ -> readings
+           |> Seq.map(fun s -> s |> int)
+           |> Seq.windowed 3
+           |> Seq.map Seq.sum
+           |> Seq.pairwise
+           |> Seq.countBy(fun (f,s) -> (s - f) > 0)
+           |> Seq.where(fun (b,i) -> b = true)
+           |> Seq.map snd
+           |> Seq.head
+
 let runDayOne =
+    // Part 1
     let testReadings = countIncreasingReadings @"test-input-1.txt"
-    printfn $"Test readings: %i{testReadings}"
+    printfn $"Test: %i{testReadings}"
     let readings = countIncreasingReadings @"prob-input-1.txt"
-    printfn $"Problem Input: %i{readings}" 
+    printfn $"Problem: %i{readings}"
+    // Part 2
+    let testWindows = countIncreasingSlidingWindows @"test-input-1-2.txt"
+    printfn $"Test: %i{testWindows}"
+    let windows = countIncreasingSlidingWindows @"prob-input-1-2.txt"
+    printfn $"Problem: %i{windows}"
+    

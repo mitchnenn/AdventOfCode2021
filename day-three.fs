dddf5c0ff830f46fde62c626dcb984c88c1c303d
Mitch Nenn <mitch.nenn@earthlink.net>
2021-12-02 22:20:19 -0800

    Some refactoring.

--- day-three.fs
+++ day-three.fs
@@ -3,14 +3,14 @@ module day_three
 open System
 open common
 
-let getMostCommon (lines:char array seq) (col:int) =
+let getCommon f (lines:char array seq) (col:int) =
     lines
     |> Seq.map(fun line -> line.[col])
     |> Seq.countBy id
-    |> Seq.maxBy(snd)
+    |> f(snd)
     |> fst
 
-let getGamma (lines:string seq) =
+let getValue f (lines:string seq) =
     let lineChars = lines |> Seq.map(fun line -> line.ToCharArray())
     let len = lineChars |> Seq.head |> Array.length
     let rec getMostCommon' index (acc:char list) =
@@ -19,34 +19,16 @@ let getGamma (lines:string seq) =
                                |> List.rev
                                |> implode
                    Convert.ToInt32(value, 2)
-        | true -> getMostCommon' (index + 1) ((getMostCommon lineChars index) :: acc)
+        | true -> getMostCommon' (index + 1) ((getCommon f lineChars index) :: acc)
     getMostCommon' 0 []
-
-let getLeastCommon (lines:char array seq) (col:int) =
-    lines
-    |> Seq.map(fun line -> line.[col])
-    |> Seq.countBy id
-    |> Seq.minBy(snd)
-    |> fst
-
-let getEpsilon (lines:string seq) =
-    let lineChars = lines |> Seq.map(fun line -> line.ToCharArray())
-    let len = lineChars |> Seq.head |> Array.length
-    let rec getLeastCommon' index (acc:char list) =
-        match index < len with
-        | false -> let value = acc
-                               |> List.rev
-                               |> implode
-                   Convert.ToInt32(value, 2)
-        | true -> getLeastCommon' (index + 1) ((getLeastCommon lineChars index) :: acc)
-    getLeastCommon' 0 []
 
 let getPowerConsumption (filename:string) =
     let lines = readLines filename
     match lines with
-    | s when Seq.isEmpty s -> 0
-    | _ -> let gamma = getGamma lines
-           let epsilon = getEpsilon lines
+    | s when Seq.isEmpty s -> -1
+    | _ ->
+           let gamma = getValue Seq.maxBy lines
+           let epsilon = getValue Seq.minBy lines
            gamma * epsilon
 
 let runDayThree =

f470d22930194f12c490c59c2ecd7b0768c9594c
Mitch Nenn <mitch.nenn@earthlink.net>
2021-12-04 09:48:17 -0800

    Start day four.

--- Program.fs
+++ Program.fs
@@ -2,11 +2,10 @@
 open day_one
 open day_two
 open day_three
+open day_four
 
 // For more information see https://aka.ms/fsharp-console-apps
 //runDayOne
 //runDayTwo
-runDayThree
-
-
-                   
+//runDayThree
+runDayFour
---
+++ day-four.fs
@@ -0,0 +1,5 @@
+module day_four
+
+
+let runDayfour =
+    ()

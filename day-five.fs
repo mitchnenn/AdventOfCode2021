9ccffa43edc542569d7e13ec8a0581ea3f049ffc
Mitch Nenn <mitch.nenn@earthlink.net>
2021-12-14 21:41:15 -0800

    Day 5 part 2 works.

--- day-five.fs
+++ day-five.fs
@@ -42,26 +42,48 @@ let getDimensions points =
     width, height
 
 let getHorizontalLines points = points |> List.where(fun (s,e) -> s.x = e.x)
-let getVerticalLines points = points |> List.where(fun (s,e) -> s.y = e.y)
 
 let getHLinePoints (line:Point * Point) : Point list =
     let ys = [fst(line).y; snd(line).y] 
     [(ys |> List.min)..(ys |> List.max)]
     |> List.map(fun y -> {x=fst(line).x; y=y})
 
-let getVLinePoints (line:Point * Point) : Point list =
-    let xs = [fst(line).x; snd(line).x]
-    [(xs |> List.min)..(xs |> List.max)]
-    |> List.map(fun x -> {x=x; y=fst(line).y})
-    
 let getAllHorizontalPoints hLines =
     hLines
     |> List.map getHLinePoints
     |> List.reduce (fun i state -> state@i)
 
+let getVerticalLines points = points |> List.where(fun (s,e) -> s.y = e.y)
+
+let getVLinePoints (line:Point * Point) : Point list =
+    let xs = [fst(line).x; snd(line).x]
+    [(xs |> List.min)..(xs |> List.max)]
+    |> List.map(fun x -> {x=x; y=fst(line).y})
+    
 let getAllVerticalPoints vLines =
     vLines
     |> List.map getVLinePoints
+    |> List.reduce (fun i state -> state@i)
+
+let getDiagonalLines points =
+    points
+    |> List.where(fun (s,e) -> abs(s.x - e.x) = abs(s.y - e.y))
+
+let enumInts first last =
+    match first - last with
+    | diff when diff > 0 -> seq { for i = first downto last do yield i }
+    | diff when diff < 0 -> seq { for i = first to last do yield i }
+    | _ -> failwith "Can't create list of ints."
+
+let getDLinePoints (line:Point * Point) : Point list =
+    let xs = enumInts (fst(line).x) (snd(line).x) |> Seq.toList
+    let ys = enumInts (fst(line).y) (snd(line).y) |> Seq.toList
+    List.zip xs ys
+    |> List.map(fun (x,y) -> {x=x; y=y})
+
+let getAllDiagonalPoints dLines =
+    dLines
+    |> List.map getDLinePoints
     |> List.reduce (fun i state -> state@i)
 
 let getDangerPointCount submarineLines =
@@ -70,19 +92,19 @@ let getDangerPointCount submarineLines =
                   |> getAllHorizontalPoints
     let vPoints = getVerticalLines points
                   |> getAllVerticalPoints
-    [hPoints;vPoints]
+    let dPoints = getDiagonalLines points
+                  |> getAllDiagonalPoints
+    [hPoints;vPoints;dPoints]
     |> List.concat
     |> List.countBy id
     |> List.where(fun (_,c) -> c >= 2)
     |> List.length
 
 let runDayFive =
-    // Part 1.
     let testSubmarineLines = parseInputFile "test-input-5-1.txt"
     let testPointsCoveredCount = getDangerPointCount testSubmarineLines
-    printfn $"%A{testPointsCoveredCount}"        
+    printfn $"%i{testPointsCoveredCount}"        
     let probSubmarineLines = parseInputFile "test-input-5-2.txt"
     let probPointsCoveredCount = getDangerPointCount probSubmarineLines
-    printfn $"%A{probPointsCoveredCount}"        
-    // Part 2.
+    printfn $"%i{probPointsCoveredCount}"        
     

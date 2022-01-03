615c8128e4eaaa1ffe5bc139807fd3f8aaa08cab
Mitch Nenn <mitch.nenn@earthlink.net>
2022-01-03 15:26:30 -0800

    Day 10 part 2 complete.

--- day-ten.fs
+++ day-ten.fs
@@ -37,6 +37,39 @@ let getCorruptCharScore illegalChar =
     | '>' -> 25137
     | _ -> failwith $"Not a valid close character: {illegalChar}"
 
+let getUnClosedDelimiters (line:string) : char list option =
+    let rec loop left openChars =
+        match left with
+        | [] ->
+            match openChars with
+            | [] -> None
+            | _ -> Some(openChars)
+        | x::xs ->
+            match x with
+            | _ when isOpenChar x -> loop xs (x::openChars)
+            | _ when openChars |> List.isEmpty -> None
+            | _ when (getOpenChar x) <> openChars.[0] -> None
+            | _ -> loop xs (openChars |> List.removeAt 0)
+    loop (line |> String.toList) []
+
+let getCompScoreValue c =
+    match c with
+    | ')' -> 1L
+    | ']' -> 2L
+    | '}' -> 3L
+    | '>' -> 4L
+    | _ -> failwith $"Unknown close char: {c}"
+
+let scoreLineCompletion (comp:char list) : int64 =
+    comp
+    |> List.fold(fun state x -> (state * 5L) + (getCompScoreValue x)) 0L
+
+let pickScore (scores:int64 list) : int64 =
+    let temp = scores |> List.sort
+    let len = scores |> List.length
+    let middle = len / 2
+    temp |> List.item middle
+
 let runDayTen =
     let testScore1 = readLines "test-day-ten-1.txt"
                      |> List.choose(findCorruptChar)
@@ -49,3 +82,17 @@ let runDayTen =
                      |> List.map(getCorruptCharScore)
                      |> List.sum
     printfn "%i" probScore1
+
+    let testScore2 = readLines "test-day-ten-1.txt"
+                     |> List.choose(getUnClosedDelimiters)
+                     |> List.map(fun l -> l |> List.map(getCloseChar))
+                     |> List.map(fun l -> l |> scoreLineCompletion)
+                     |> pickScore
+    printfn "%A" testScore2
+    
+    let probScore2 = readLines "prob-day-ten-1.txt"
+                     |> List.choose(getUnClosedDelimiters)
+                     |> List.map(fun l -> l |> List.map(getCloseChar))
+                     |> List.map(fun l -> l |> scoreLineCompletion)
+                     |> pickScore
+    printfn "%A" probScore2    
